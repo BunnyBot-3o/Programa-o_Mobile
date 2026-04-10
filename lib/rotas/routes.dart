8e9fd53c9b9c2afa9b32750541608b3b4c24c7f0
O Anão Vascaino Bombado <d.brunetto@escola.pr.gov.br>
2026-04-10 12:48:10 +0000

    Criando navegação no app

---
+++ lib/rotas/routes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/My_Home_Page.dart';
+import 'package:flutter_app/paginas/aula01.dart';
+
+final Map<String,  WidgetBuilder> appRoutes = {
+  "/": (context) => const  MyHomePage(title: 'Página Principal'),
+  "/aula01": (context) => const Aula01(),
+
+}
