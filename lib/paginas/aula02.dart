c5a6e061812bc49083c4c1e1386902a48559a0c0
O Anão Vascaino Bombado <d.brunetto@escola.pr.gov.br>
2026-08-21 12:40:30 +0000

    alinhamentos

--- lib/paginas/aula02.dart
+++ lib/paginas/aula02.dart
@@ -1,16 +1,95 @@
 import 'package:flutter/material.dart';
 
-class aula02 extends StatelessWidget {
-    const aula02 ({super.key});
+class Aula02 extends StatelessWidget {
+  const Aula02({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(appBar: AppBar(
-            title: Text('Aula 02 - rows e colums'),
+  @override
+  Widget build(BuildContext context) {
+    // construindo widget customisado (reaproveitamento do codigo)
+    Widget _construirLinha(MainAxisAlignment alinhamento,
+     IconData icone1,
+     IconData icone2,
+     IconData icone3,
+     ) {
+      return Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.red),
         ),
-        body: Center(
-            child: Text("conteúdo da aula 02"),
-        )
+        // Criando uma linha que alinha seu conteúdo no início
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.green, width: 2),
+              ),
+              child: Icon(Icons.one_k_outlined),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.green, width: 2),
+              ),
+              child: Icon(Icons.access_time_filled_rounded),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.green, width: 2),
+              ),
+              child: Icon(Icons.account_tree_sharp),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Aula 02 - Rows e Columns"),
+      ),
+      // Cria uma barra de rolagem para o conteúdo da tela
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+              child: Text("Start (Padrão)"),
+            ),
+            
+            // definindo uma coluna
+           Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: const Color.fromARGB(255, 54, 67, 244)
+                ),
+              ), height: 200,
+              // Criando uma coluna que alinha seu conteúdo no início
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green, width: 2),
+                    ),
+                    child: Icon(Icons.one_k_outlined),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green, width: 2),
+                    ),
+                    child: Icon(Icons.access_time_filled_rounded),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green, width: 2),
+                    ),
+                    child: Icon(Icons.account_tree_sharp),
+                  ),
+                ],
+              ),
+            ),],
+        ),
+      ),
     );
   }
 }
--- lib/rotas/routes.dart
+++ lib/rotas/routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/my_home_page.dart';
 import 'package:flutter_app/paginas/aula01.dart';
+import 'package:flutter_app/paginas/aula02.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
   "/": (context) => const MyHomePage(title: 'Página Principal'),
